d3d0a4eed2c90ec466eccccc944f5b6cfa32ca3c
elz <elzj78@gmail.com>
2019-09-06 15:45:38 -0400

    Add basic tag validations

--- app/models/tags/tag.rb
+++ app/models/tags/tag.rb
@@ -1,2 +1,24 @@
 class Tag < ApplicationRecord
+  TAGGABLE_TYPES = %w(Rating Warning Category Character Relationship Freeform).freeze
+
+  ### ASSOCIATIONS
+  has_many :taggings, foreign_key: :tagger_id
+
+  ### VALIDATIONS
+  validates :name,
+    presence: true,
+    uniqueness: { case_sensitive: false },
+    length: {
+      minimum: ArchiveConfig.tags[:name_min],
+      maximum: ArchiveConfig.tags[:name_max]
+    }
+
+  validates :type,
+    inclusion: { in: TAGGABLE_TYPES }
+
+  ### CALLBACKS
+
+  ### CLASS METHODS
+
+  ### INSTANCE METHODS
 end
