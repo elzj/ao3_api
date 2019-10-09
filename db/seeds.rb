e32b56eae256fade570ffa7e8207227a40e2fbd6
elz <elzj78@gmail.com>
2019-10-09 10:59:35 -0400

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Language.create(short: 'en', name: 'English')
+
+Warning::DEFAULTS.each { |name| Warning.create(name: name, canonical: true) }
+Rating::DEFAULTS.each { |name| Rating.create(name: name, canonical: true) }
+Category::DEFAULTS.each { |name| Category.create(name: name, canonical: true) }
